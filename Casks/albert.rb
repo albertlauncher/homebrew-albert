803f01d3d9f5242c2c6b748ebf4b408abe400525
Manuel Schneider <manuelschneid3r@gmail.com>
2026-09-12 14:29:05 +0200

    Fix homebrew deprecation warings

--- Casks/albert.rb
+++ Casks/albert.rb
@@ -5,8 +5,7 @@ cask "albert" do
   version "35.1.0"
   sha256 arm: "b23543a0a2c99f4950c2d24eb394d39c84faf52dfa0665b01a2dfa925e7fc041", intel: "c7fd5eebea560a5aa4cd73eb5d532113a1b952ed1e7cb3668f0b703f9db4ab15"
 
-  url "https://github.com/albertlauncher/albert/releases/download/v#{version}/Albert-v#{version}-#{arch}.dmg",
-      verified: "github.com/albertlauncher/albert/"
+  url "https://github.com/albertlauncher/albert/releases/download/v#{version}/Albert-v#{version}-#{arch}.dmg"
   name "Albert"
   desc "Plugin-based keyboard launcher"
   homepage "https://albertlauncher.github.io/"
@@ -23,7 +22,7 @@ cask "albert" do
 
   app "Albert.app"
 
-  postflight do
+  postflight_steps do
 
     system_command "xattr",
              args: ["-d", "com.apple.quarantine", "/Applications/Albert.app"],
